c71bd77760138c0a7c749030021d2c97c7c8facc
Desacratrix <45645382+Desacratrix@users.noreply.github.com>
2019-07-03 20:58:59 +0300

    add button to play and styling

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var randomNumber1 = Math.floor((Math.random()*6)+1);
-var randomImgName1 = "images/dice"+randomNumber1+".png";
-var leftImage = document.querySelector(".img1");
-leftImage.setAttribute("src", randomImgName1);
 
-var randomNumber2 = Math.floor((Math.random()*6)+1);
-var randomImgName2 = "images/dice"+randomNumber2+".png";
-var rightImage = document.querySelector(".img2");
-rightImage.setAttribute("src", randomImgName2);
-
-var header = document.querySelector("h1");
 
 function winner() {
+  var randomNumber1 = Math.floor((Math.random()*6)+1);
+  var randomImgName1 = "images/dice"+randomNumber1+".png";
+  var leftImage = document.querySelector(".img1");
+  leftImage.setAttribute("src", randomImgName1);
+
+  var randomNumber2 = Math.floor((Math.random()*6)+1);
+  var randomImgName2 = "images/dice"+randomNumber2+".png";
+  var rightImage = document.querySelector(".img2");
+  rightImage.setAttribute("src", randomImgName2);
+
+  var header = document.querySelector("h1");
 
   if (randomNumber1 === randomNumber2) {
     header.innerText = "It's a draw, play again!";
